4174c91973d22930942c7838cff882184cdf1efb
kalenduhs <kalenduhs@gmail.com>
2024-01-17 20:32:58 -0600

    refactored the table for more input

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -8,18 +8,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
+    //private static List<String> events = new ArrayList<>();
+    private HashMap<String, String> events = new HashMap<>();
+//     events.put("Menteaship","A fun meetup for connecting with mentors");
+//     events.put("Code With Pride","A fun meetup sponsored by LaunchCode");
+//     events.put("Javascripty", "An imaginary meetup for Javascript developers");
 
     @GetMapping
     public String methodName(Model model) {
         model.addAttribute("title", "All Events");
         model.addAttribute("events", events);
+        events.put("Menteaship","A fun meetup for connecting with mentors");
+        events.put("Code With Pride","A fun meetup sponsored by LaunchCode");
+        events.put("Javascripty", "An imaginary meetup for Javascript developers");
         return "events/index";
     }
 
@@ -30,8 +38,8 @@ public class EventController {
     }
 
     @PostMapping("create")
-    public String methodNameThree(@RequestParam String eventName) {
-        events.add(eventName);
+    public String methodNameThree(@RequestParam String eventName, String eventDescription) {
+        events.put(eventName, eventDescription);
         return "redirect:/events"; //needs the /events
     }
 }
